c679f2e4b1d06ecbe4188445c1485e2635f28eba
Victor Botelho Silva <victorbotelhosilva@proton.me>
2026-07-10 15:50:10 -0300

    feat: added desktop host with hardware-configuration.nix

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -1,1 +1,15 @@
-{}
+{ ... }:
+
+{
+  imports = [
+    ../../profiles/base.nix
+    ../../modules/editors/neovim.nix
+    #../../modules/desktops/plasma.nix
+    ../../modules/desktops/hyprland.nix
+    ../../modules/browsers/firefox.nix
+    ./hardware-configuration.nix
+  ];
+
+  networking.hostName = "nix-desktop";
+  system.stateVersion = "26.05";
+}
