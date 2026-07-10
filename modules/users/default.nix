cb9d997362ba1c4039267d298edd8dc0e3a42c11
Victor Botelho Silva <victorbotelhosilva@proton.me>
2026-07-10 15:51:21 -0300

    feat: added a default password for the main user

--- modules/users/default.nix
+++ modules/users/default.nix
@@ -5,6 +5,7 @@
     isNormalUser = true;
     description = "Victor Botelho Silva";
     extraGroups = [ "wheel" "docker" "libvirtd" "kvm" "video" "input" "networkmanager" ];
+	initialPassword = "1234567890";
 	shell = pkgs.bash;
   };
 }
