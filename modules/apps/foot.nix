0066ac764a40d24d28977019cb87b5e888ae79c1
Victor Botelho Silva <victorbotelhosilva@proton.me>
2026-07-11 21:09:27 -0300

    fix: fixed foot config

--- modules/apps/foot.nix
+++ modules/apps/foot.nix
@@ -1,7 +1,11 @@
-{ pkgs, ... }:
+{ pkgs, user, ... }:
 
 {
   environment.systemPackages = with pkgs; [
     foot
   ];
+
+  home-manager.users.${user} = { config, ... }: {
+    home.file.".config/foot".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos/config/foot";
+  };
 }
